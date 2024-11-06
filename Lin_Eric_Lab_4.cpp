77df0baaaf4f988a5defd7fc8c5e1851fb210b2d
ericl343 <ericl343@nycstudents.net>
2024-11-05 23:27:21 -0500

    2

--- Lin_Eric_Lab_4.cpp
+++ Lin_Eric_Lab_4.cpp
@@ -2,39 +2,68 @@
 //Lab_4
 //October_1_2024
 
-#include <iostream>                 //Crucial Piece
-#include <string>                   //Also Crucial
+#include <iostream>                 // Crucial Piece
+#include <string>                   // Also Crucial
 
-using namespace std;                //Crucial Too
+using namespace std;                // Crucial Too
 
-int main() {                                                                                //Start of Function
+int main() 
+{  
     string fruitN;
     double gramSug;
+    char userChoice;  // Variable to ask if the user wants to continue
     
-    //Questions to ask user for data purposes
+    // Questions to ask user for data purposes
     cout << "What is your fruit: " << endl;   
-    cin >> fruitN;                                                                          //Name of Fruit
+    // Name of Fruit
+    cin >> fruitN;                                                                          
     cout << "How many grams of sugar does one cup of " << fruitN << " contain: " << endl;   
-    cin >> gramSug;                                                                         //Unit of Sugar 
+    // Unit of Sugar 
+    cin >> gramSug;                                                                         
 
-    //Following is for looping
-    while (true) 
-    { 
+    // Following is for looping
+    while (true) { 
+        // If else statements
+        if (gramSug < 10) { 
+            cout << fruitN << " is a LOW SUGAR FRUIT." << endl; 
+        }
+        else if (gramSug >= 10 && gramSug <= 14) { 
+            cout << fruitN << " is a MEDIUM SUGAR FRUIT." << endl; 
+        }
+        else if (gramSug > 14) {
+            cout << fruitN << " is a HIGH SUGAR FRUIT." << endl;
+        }
 
-    //If else statements
-    if (gramSug < 10) 
-    { 
-    cout << fruitN << " is a LOW SUGAR FRUIT." << endl; 
+        // Ask if the user wants to continue
+        cout << "Would you like to enter another fruit? (y/n): ";
+        cin >> userChoice;
+
+        // Exit the loop if the user chooses 'n' or 'N'
+        if (userChoice == 'n' || userChoice == 'N') {
+            break;  
+        }
+
+        // If the user wants to continue, ask for new fruit data
+        cout << "What is your fruit: " << endl;
+        cin >> fruitN;
+        cout << "How many grams of sugar does one cup of " << fruitN << " contain: " << endl;
+        cin >> gramSug;
     }
-    else if (gramSug >= 10 && gramSug <= 14) 
-    { 
-    cout << fruitN << " is a MEDIUM SUGAR FRUIT." << endl; 
-    }
-    else if (gramSug > 14) 
-    {
-    cout << fruitN << " is a HIGH SUGAR FRUIT." << endl;
-    }
-    }
-    //End of Code
+
+    // End of Code
     return 0;
 }
+/*
+What is your fruit: 
+Cherry
+How many grams of sugar does one cup of Cherry contain: 
+9
+Cherry is a LOW SUGAR FRUIT.
+Would you like to enter another fruit? (y/n): y
+What is your fruit: 
+Strawberry
+How many grams of sugar does one cup of Strawberry contain: 
+13
+Strawberry is a MEDIUM SUGAR FRUIT.
+Would you like to enter another fruit? (y/n): n
+*/
